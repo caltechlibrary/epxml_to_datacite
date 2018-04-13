35664b74daf62dbebe4b841c9a39c1765580acd6
Tom Morrell <tmorrell@caltech.edu>
2018-04-13 15:46:44 -0700

    Skeleton

--- caltech_thesis.py
+++ caltech_thesis.py
@@ -1,4 +1,5 @@
 import xmltodict
+from datacite import schema40
 import glob,json
 
 files = glob.glob('*.xml')
@@ -6,4 +7,15 @@ for f in files:
     with open(f) as fd:
         eprint = xmltodict.parse(fd.read())['eprints']['eprint']
     print(eprint['title'])
+
+    metadata = eprint
+
+    assert schema40.validate(metadata)
+    #Debugging if this fails
+    #v = schema40.validator.validate(metadata)
+    #errors = sorted(v.iter_errors(instance), key=lambda e: e.path)
+    #for error in errors:
+    #        print(error.message)
+
+    xml = schema40.tostring(metadata)
     #print(json.dumps(eprint))
